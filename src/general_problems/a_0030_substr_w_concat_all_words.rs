8d232536e07c87a7c8f859da22ebf60abf32f975
dbrowne <dwight@dwightjbrowne.com>
2023-05-24 20:23:45 -0700

    prob_30

--- src/general_problems/a_0030_substr_w_concat_all_words.rs
+++ src/general_problems/a_0030_substr_w_concat_all_words.rs
@@ -46,30 +46,85 @@ Constraints:
 s and words[i] consist of lowercase English letters.*/
 
 
-pub  mod  a30{
+pub mod a30 {
+    use std::collections::HashMap;
 
-    pub  fn  find_substring(s: String, words:Vec<String>) ->Vec<i32>{
+    pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+        if words.is_empty() || s.is_empty() {
+            return vec![];
+        }
+        let s_l = s.len();
+        let w_l = words.len();  // word array
+        let token_len = words[0].len();
 
+        if s_l < token_len * w_l {
+            return vec![];
+        }
+        let mut word_freq = words.iter().fold(HashMap::new(), |mut word_hash, word| {
+            *word_hash.entry(&word[..]).or_insert(0) += 1;
+            word_hash
+        });
 
-
-        vec![0]
+        let mut ans = vec![];
+        let mut idx = 0;
+        for idx in 0..token_len {
+            let mut tmp_freq = word_freq.clone();
+            let mut good = 0;
+            // start the sliding window
+            for ctr in (idx..s_l - token_len + 1).step_by(token_len) {
+                let c_word = &s[ctr..ctr + token_len];
+                if let Some(crumb) = tmp_freq.get(c_word) {
+                    if *crumb > 0 {
+                        good += 1;
+                    }
+                }
+                *tmp_freq.entry(c_word).or_default() -= 1;
+                // reduce the window size
+                if ctr >= w_l * token_len {
+                    let i = ctr - w_l * token_len;
+                    let p_word = &s[i..i + token_len];
+                    let count = tmp_freq.entry(p_word).or_default();
+                    *count += 1;
+                    if *count > 0 && good > 0 {
+                        good -= 1;
+                    }
+                }
+                if good == w_l {
+                    ans.push((ctr + token_len - w_l * token_len) as i32);
+                }
+            }
+        }
+        ans
     }
 }
 
 
 #[cfg(test)]
-mod test{
+mod test {
     use crate::general_problems::a_0030_substr_w_concat_all_words::a30::find_substring;
 
     #[test]
-    fn t_0(){
-        let  s = "barfoothefoobarman".to_string();
-        let  words = vec!["foo".to_string(), "bar".to_string()];
+    fn t_0() {
+        let s = "barfoothefoobarman".to_string();
+        let words = vec!["foo".to_string(), "bar".to_string()];
 
-        assert_eq!(vec![0,9],find_substring(s,words) );
+        assert_eq!(vec![0, 9], find_substring(s, words));
     }
 
-
+    #[test]
+    fn t_1(){
+        let  s ="wordgoodgoodgoodbestword".to_string();
+        let words =vec!["word".to_string(),"good".to_string(),"best".to_string(),"word".to_string()];
+        let  ans:Vec<i32> =vec![];
+        assert_eq!(ans, find_substring(s,words));
+    }
+    #[test]
+    fn t_2(){
+        let  s = "barfoofoobarthefoobarman".to_string();
+        let  words = vec!["bar".to_string(),"foo".to_string(), "the".to_string()];
+        let  ans:Vec<i32> = vec![6,9,12];
+        assert_eq!(ans, find_substring(s,words));
+    }
 }
 
 
